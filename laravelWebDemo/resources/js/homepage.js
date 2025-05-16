744401d4b39b0cb035d6bf95393afdd977fafada
I-amPrashant <airmax50cent@gmail.com>
2025-05-16 20:28:08 +0545

    add popup on refresh on homepage- laravelWebDemo

--- laravelWebDemo/resources/js/homepage.js
+++ laravelWebDemo/resources/js/homepage.js
@@ -1,20 +1,51 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
     //responsive nav
-    const navbarWrapper=document.getElementById('navbar-item-wrapper');
-    const hamburger=document.getElementById('hamburger');
+    const navbarWrapper = document.getElementById("navbar-item-wrapper");
+    const hamburger = document.getElementById("hamburger");
 
-    hamburger.addEventListener('click', () => {
-        navbarWrapper.classList.toggle('h-0');
-        navbarWrapper.classList.toggle('h-[280px]');
+    hamburger.addEventListener("click", () => {
+        navbarWrapper.classList.toggle("h-0");
+        navbarWrapper.classList.toggle("h-[280px]");
     });
 
-    document.documentElement.addEventListener('click', (e) => {
-        if(!e.target.closest('#hamburger')){
-            navbarWrapper.classList.add('h-0');
-            navbarWrapper.classList.remove('h-[280px]');
+    document.documentElement.addEventListener("click", (e) => {
+        if (!e.target.closest("#hamburger")) {
+            navbarWrapper.classList.add("h-0");
+            navbarWrapper.classList.remove("h-[280px]");
         }
-        //or 
+        //or
         // !e.target.closest('#hamburger')// this will return true or false as per the element is sibling or child of #hamburger
     });
 
+    //popup javascript
+    const popupContainer = document.querySelector("#popup-container");
+    const popupCloseButton = document.querySelector("#popup-close-button");
+    const popup = document.querySelector("#popup");
+
+    if (performance.getEntriesByType("navigation")[0].type === "reload" && window.location.pathname === "/") {
+        setTimeout(() => {
+            popupContainer.style.display = "block";
+
+            popupContainer.animate([{ opacity: 0 }, { opacity: 1 }], {
+                duration: 300,
+                easing: "ease-out",
+                fill: "forwards",
+            });
+            popup.animate(
+                [
+                    { transform: "translateY(-20%)", opacity: 0 },
+                    { transform: "translateY(0)", opacity: 1 },
+                ],
+                {
+                    duration: 200,
+                    easing: "ease-in",
+                    fill: "forwards",
+                }
+            );
+        }, 1500);
+    }
+
+    popupCloseButton.addEventListener("click", () => {
+        popupContainer.style.display = "none";
+    });
 });
--- laravelWebDemo/resources/views/home.blade.php
+++ laravelWebDemo/resources/views/home.blade.php
@@ -6,8 +6,25 @@
 @section('title', 'Home page')
 
 @section('hero')
-    <div>
-        <h1>Home page</h1>
-        <p>it has dynamic contents</p>
-    </div>
+
+            <!-- popup on page reload -->
+        <div class="hidden h-screen w-screen absolute top-0 z-100 left-0 px-4" id="popup-container">
+
+            <!-- overlay  -->
+             <div class="absolute top-0 left-0 w-full h-full bg-gray-900 opacity-50"></div>
+
+                <div class="relative top-[100px] left-[50%] -translate-x-1/2 bg-white px-12 py-10 max-w-[500px]" id="popup">
+                    <!-- cross button  -->
+                    <button class="absolute top-3 right-4 text-xl text-gray-800 hover:text-black cursor-pointer" id="popup-close-button"><img class="h-4 w-4 sm:w-5 sm:h-5" src="{{ asset('images/cross.svg') }}" alt="cross"></button>
+                    
+                    <!-- content  -->
+                     <h1 class=" text-lg sm:text-2xl text-center">Notice of election for the post of senior developer in government sector</h1>
+                     <p class="text-sm mt-5 break-all sm:text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia perferendis voluptates, suscipit possimus molestias ipsa soluta aperiam explicabo labore laudantium totam consectetur quia, vel in exercitationem. Aliquid nisi consequatur nemo!</p>
+
+                     <p></p>
+                </div>  
+        </div>
+
+        <!-- hero section  -->
+
 @endsection
